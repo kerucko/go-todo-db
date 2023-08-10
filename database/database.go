9219fb3a2c4234d1e393d53f60cd6ccd02186659
kerucko <cykinpavel@gmail.com>
2023-08-10 17:39:36 +0300

    config

--- database/database.go
+++ database/database.go
@@ -2,20 +2,43 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 var DB *sql.DB
 
+type config struct {
+	Host     string `yaml:"host"`
+	Port     string `yaml:"port"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	Database string `yaml:"database"`
+}
+
+var c config
+
 func Init() {
 	var err error
-	DB, err = sql.Open("mysql", "root:mysql_password1@tcp(localhost:3306)/todo")
+	readConfig()
+	//log.Println(c)
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", c.User, c.Password, c.Host, c.Port, c.Database)
+	//log.Println(connection)
+	DB, err = sql.Open("mysql", connection)
 	if err != nil {
 		panic(err)
 	}
 	//defer DB.Close()
 
 	err = DB.Ping()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readConfig() {
+	err := cleanenv.ReadConfig("config.yml", &c)
 	if err != nil {
 		panic(err)
 	}
